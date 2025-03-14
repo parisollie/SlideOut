0f00bf1742db9e0e0ab90b4a93e0aff372a94faa
parisollie <pjff18@gmail.com>
2025-03-13 22:24:17 -0600

    Initial Commit

---
+++ SlideOut/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  SlideOut
+//
+//  Created by Paul F on 13/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        BaseView()
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SlideOut/SlideOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideOutApp.swift
+//  SlideOut
+//
+//  Created by Paul F on 13/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SlideOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
